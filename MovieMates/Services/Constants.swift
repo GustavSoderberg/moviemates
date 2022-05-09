9d2fd888d21e3ad9ce2dc37b3d988181033ca4f3
piggest <piggest@mail.ru>
2022-05-09 11:48:00 +0200

    add images im movie card view

--- MovieMates/Services/Constants.swift
+++ MovieMates/Services/Constants.swift
@@ -14,3 +14,4 @@ let BASE_API_URL = "https://api.themoviedb.org/3/"
 let HEADER: HTTPHeaders  = [
     "Content-Type" : "application/json; charset=utf-8"
 ]
+let BACKDROP_BASE_URL  = "https://image.tmdb.org/t/p/w185"
