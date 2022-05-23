6c22378669274558914a790e6d6e21b83fa5f683
Oscar <oscarkarlsso@live.com>
2022-05-23 12:26:42 +0200

    reviewViews all use the same code now

--- MovieMates/Views/HomeView.swift
+++ MovieMates/Views/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
     
     @State var index = "friends"
     @State var showMovieView = false
-    @State var presentMovie: Movie? = nil
+    @State var currentMovie: Movie? = nil
     
     @ObservedObject var viewModel = MovieListViewModel()
     @ObservedObject var allReviewsViewModel = ReviewListViewModel()
@@ -41,11 +41,11 @@ struct HomeView: View {
                         switch index {
                         case FRIENDS:
                             ForEach(friendsReviewsViewModel.reviews) { review in
-                                ReviewCardView(review: review, movieFS: rm.getMovieFS(movieId: "\(review.movieId)"), presentMovie: $presentMovie, showMovieView: $showMovieView)
+                                ReviewCard(review: review, movieFS: rm.getMovieFS(movieId: "\(review.movieId)"), currentMovie: $currentMovie, showMovieView: $showMovieView, displayName: true, displayTitle: true)
                             }
                         case TRENDING:
                             ForEach(allReviewsViewModel.reviews) { review in
-                                ReviewCardView(review: review, movieFS: rm.getMovieFS(movieId: "\(review.movieId)"), presentMovie: $presentMovie, showMovieView: $showMovieView)
+                                ReviewCard(review: review, movieFS: rm.getMovieFS(movieId: "\(review.movieId)"), currentMovie: $currentMovie, showMovieView: $showMovieView, displayName: true, displayTitle: true)
                             }
                         case POPULAR:
                             ForEach(viewModel.movies) { movie in
@@ -64,7 +64,7 @@ struct HomeView: View {
                             
                         default:
                             ForEach(friendsReviews) { review in
-                                ReviewCardView(review: review, presentMovie: $presentMovie, showMovieView: $showMovieView)
+                                ReviewCard(review: review, currentMovie: $currentMovie, showMovieView: $showMovieView, displayName: true, displayTitle: true)
                             }
                         }
                     }
@@ -74,8 +74,8 @@ struct HomeView: View {
 //                    }
                     .padding()
                     .sheet(isPresented: $showMovieView) {
-                        if let presentMovie = presentMovie {
-                            MovieViewController(movie: presentMovie, showMovieView: $showMovieView)
+                        if let currentMovie = currentMovie {
+                            MovieViewController(movie: currentMovie, showMovieView: $showMovieView)
                                 .preferredColorScheme(darkmode ? .dark : .light)
                         }
                     }
@@ -104,87 +104,6 @@ struct HomeView: View {
     }
 }
 
-struct ReviewCardView: View {
-    
-    let review: Review
-    var movieFS: MovieFS?
-    @Binding var presentMovie: Movie?
-    @Binding var showMovieView : Bool
-    
-    @State private var isExpanded: Bool = false
-    
-    private let movieViewModel: MovieViewModel = MovieViewModel.shared
-    
-    var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color("secondary-background"))
-            HStack(alignment: .top){
-                if let movie = movieFS {
-                    
-                    AsyncImage(url: movie.photoUrl){ image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 100, height: 150, alignment: .center)
-                    .border(Color.black, width: 3)
-                    .onTapGesture {
-                        
-                        print("click!")
-                        loadMovie(id: movie.id!)
-                        showMovieView = true
-                        //um.refresh += 1
-                    }
-                    
-                    VStack(alignment: .leading){
-                        
-                        HStack{
-                            Text(um.getUser(id: review.authorId).username)
-                            Spacer()
-                            Text(formatDate(date: review.timestamp))
-                                .font(.system(size: 12))
-                        }
-                        
-                        Text(movie.title)
-                            .font(.title2)
-                            .minimumScaleFactor(0.7)
-                            .lineLimit(1)
-                        
-                        Text("\(review.rating)")
-                            .padding(.bottom, 4)
-                        
-                        Text(review.reviewText)
-                            .font(.system(size: 15))
-                            .lineLimit(isExpanded ? nil : 4)
-                            .onTapGesture {
-                                isExpanded.toggle()
-                            }
-                    }
-                }
-            }
-            .padding()
-        }
-    }
-    func loadMovie(id: String) {
-        presentMovie = nil
-        movieViewModel.fetchMovie(id: Int(id)!) { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .failure(let error):
-                    print(error)
-                case .success(let movie):
-                    presentMovie = movie
-                }
-            }
-        }
-    }
-}
-
-
-
 func formatDate(date: Date) -> String{
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "d MMMM yyyy"
@@ -192,23 +111,7 @@ func formatDate(date: Date) -> String{
 }
 
 
-private var friendsReviews = [Review
-//    Review(movieId: 10612, username: "Sarah", title: "The Batman", rating: "5/5", reviewText: "Siken film! jag grät, jag skrek, jag belv en helt ny människa!"),
-//    Review(movieId: 634649, username: "Oscar", title: "The Duckman", rating: "4/5", reviewText: "Jag gillar ankor så denna film var helt perfekt för mig! Dock så var det ett himla kvackande i biosalongen."),
-//    Review(movieId: 364, username: "Joakim", title: "The Birdman", rating: "1/5", reviewText: "Trodde filmen skulle handla om en fågel som ville bli människa, men det var ju helt fel! Den handlar om en man som trodde han var en fågel. Falsk marknadsföring!"),
-//    Review(movieId: 414, username: "Gustav", title: "The Spiderman", rating: "5/5", reviewText: "Jag somnade efter 30min och vaknade strax innan slutet. Bästa tuppluren jag haft på länge! Rekomenderas starkt!")
-]()
+private var friendsReviews = [Review]()
 
-private var trendingReviews = [Review
-//    Review(movieId: 414906, username: "Oscar", title: "The Duckman", rating: "4/5", reviewText: "Jag gillar ankor så denna film var helt perfekt för mig! Dock så var det ett himla kvackande i biosalongen."),
-//    Review(movieId: 284052, username: "Joakim", title: "The Birdman", rating: "1/5", reviewText: "Trodde filmen skulle handla om en fågel som ville bli människa, men det var ju helt fel! Den handlar om en man som trodde han var en fågel. Falsk marknadsföring!"),
-//    Review(movieId: 406759, username: "Gustav", title: "The Spiderman", rating: "5/5", reviewText: "Jag somnade efter 30min och vaknade strax innan slutet. Bästa tuppluren jag haft på länge! Rekomenderas starkt!"),
-//    Review(movieId: 414906, username: "Sarah", title: "The Batman", rating: "5/5", reviewText: "Siken film! jag grät, jag skrek, jag belv en helt ny människa!")
-]()
-
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//    }
-//}
+private var trendingReviews = [Review]()
  
---
+++ MovieMates/Views/ViewComponents/ReviewView.swift
@@ -0,0 +1,153 @@
+//
+//  ReviewView.swift
+//  MovieMates
+//
+//  Created by Oscar Karlsson on 2022-05-23.
+//
+
+import SwiftUI
+
+struct ReviewCard: View {
+    let review: Review
+    var movieFS: MovieFS?
+    @Binding var currentMovie: Movie?
+    @Binding var showMovieView : Bool
+    let displayName: Bool
+    let displayTitle: Bool
+    
+    private let movieViewModel: MovieViewModel = MovieViewModel.shared
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(Color("secondary-background"))
+            
+            
+            HStack {
+                if let movie = movieFS {
+                    AsyncImage(url: movie.photoUrl){ image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 100, height: 150, alignment: .center)
+                    .border(Color.black, width: 3)
+                    .onTapGesture {
+                        loadMovie(id: movie.id!)
+                        showMovieView = true
+                    }
+                }
+                
+                VStack(spacing: 0) {
+                    ReviewInfo(review: review, displayName: displayName, displayTitle: displayTitle)
+                    Spacer()
+                }
+            }
+            .padding()
+        }
+    }
+    
+    func loadMovie(id: String) {
+        currentMovie = nil
+        movieViewModel.fetchMovie(id: Int(id)!) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let movie):
+                    currentMovie = movie
+                }
+            }
+        }
+    }
+}
+
+
+
+struct ReviewInfo: View {
+    let review: Review
+    let displayName: Bool
+    let displayTitle: Bool
+    @State var fullText = false
+    @State var lineLimit = 5
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 5){
+                HStack{
+                    if displayName {
+                        Text(um.getUser(id: review.authorId).username)
+                    } else {
+                        Text(um.getMovie(movieID: String(review.movieId))!.title)
+                    }
+                    Spacer()
+                    Text(formatDate(date: review.timestamp))
+                        .font(.system(size: 12))
+                }
+                if displayTitle && displayName {
+                    Text(um.getMovie(movieID: String(review.movieId))!.title)
+                }
+                HStack{
+                    ForEach(1..<6) { i in
+                        ClapperImage(pos: i, score: "\(review.rating)")
+                    }
+                    Spacer()
+                }
+                Text(review.reviewText)
+                    .font(.system(size: 15))
+                    .lineLimit(lineLimit)
+                    .onTapGesture {
+                        withAnimation() {
+                            if !fullText {
+                                fullText = true
+                                lineLimit = .max
+                            } else {
+                                fullText = false
+                                lineLimit = 5
+                            }
+                        }
+                    }
+                
+                HStack {
+                    if review.whereAt != "" || review.withWho != "" {
+                        if review.whereAt == "home" {
+                            Image(systemName: "house.circle")
+                        } else if review.whereAt == "cinema" {
+                            Image(systemName: "film.circle")
+                        }
+                        
+                        if review.withWho == "alone" {
+                            Image(systemName: "person.circle")
+                        } else if review.withWho == "friends" {
+                            Image(systemName: "person.2.circle")
+                        }
+                    } else {
+                        Text("")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ClapperImage: View {
+    var pos : Int
+    var score : String
+    @State var filled : Bool = false
+    
+    var body: some View {
+        Image("clapper-big")
+            .resizable()
+            .frame(width: 20, height: 20)
+            .foregroundColor(filled ? .black : .white)
+            .onAppear(perform: {
+                if Int(score.prefix(1)) ?? 0 >= pos {
+                    filled = true
+                } else {
+                    filled = false
+                }
+            })
+    }
+}
