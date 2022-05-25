74d925b48e6d433b2233e439b8525e689b22feb3
Oscar <oscarkarlsso@live.com>
2022-05-25 15:03:10 +0200

    test 2

--- MovieMates/Views/ViewComponents/ReviewView.swift
+++ MovieMates/Views/ViewComponents/ReviewView.swift
@@ -26,7 +26,6 @@ struct ReviewCard: View {
     private let movieViewModel: MovieViewModel = MovieViewModel.shared
     
     var body: some View {
-        //Test line
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color("welcome-clapper-top") , Color("welcome-clapper-bottom")]), startPoint: .top, endPoint: .bottom)
                 .mask(RoundedRectangle(cornerRadius: 25, style: .continuous))
